d869814f673fe26eb40d3d9b69ca7e6f1f2a90f3
Giovanni Rella <giomrella@gmail.com>
2020-12-09 17:51:48 -0500

    still working, less copypasta code

--- popup.js
+++ popup.js
@@ -3,72 +3,61 @@ const getAllValidMediaTagsCodeString = "[...document.getElementsByTagName('video
 
 //query active tab to find audio or video tags with active src and create a volume slider for each one
 runCodeOnCurrentTab(
-getAllValidMediaTagsCodeString+".map(e=> {const id = `${e.tagName}_${e.srcObject.id}`; e.id = id;return { id, volume: e.volume}})"
-, 
-	ret => {
-		ret = ret.pop();//always seems to wrap return in an array
-		//!ret || !ret.length ? createLabelForNoMediaTags() : ret.forEach(mediaTag => createSliderPerMediaTag(mediaTag));
-		if(!ret || !ret.length){
-			createLabelForNoMediaTags();
-		} else {
-			createMasterSlider();
-			ret.forEach(mediaTag => createSliderPerMediaTag(mediaTag));
-		}
+    getAllValidMediaTagsCodeString + ".map(e=> {const id = `${e.tagName}_${e.srcObject.id}`; e.id = id;return { id, volume: e.volume}})"
+    ,
+    ret => {
+        ret = ret.pop();//always seems to wrap return in an array
+        //!ret || !ret.length ? createLabel('No video or audio tags found') : ret.forEach(mediaTag => createSliderPerMediaTag(mediaTag));
+        if (!ret || !ret.length) {
+            createLabel('No video or audio tags found','white-space: nowrap;');
+        } else {
+            //createMasterSlider();
+            createLabel('Master Slider');
+            createSlider('slider-master', 1,
+                value => {
+                    [...document.getElementsByTagName('input')].forEach(e => { if (e.type === 'range') { e.value = value } });
+                    runCodeOnCurrentTab(getAllValidMediaTagsCodeString + `.forEach(e=>e.volume=${value})`, () => { });
+                }
+            );
+            //ret.forEach(mediaTag => createSliderPerMediaTag(mediaTag));
+            ret.forEach(mediaTag => {
+                createLabel(mediaTag.id);
+                createSlider(mediaTag.id, mediaTag.volume, value => runCodeOnCurrentTab(`document.getElementById('${mediaTag.id}').volume = ${value}`, () => { }));
+            });
+        }
 
-	}
+    }
 );
 
-function createLabelForNoMediaTags() {
-	const label = document.createElement('label');
-	label.innerHTML = 'No audio or video tags found on this page';
-	label.style = 'white-space: nowrap;';
-	document.body.appendChild(label);
+function createLabel(text, style='') {
+    const label = document.createElement('label');
+    label.innerHTML = text;
+    label.style = style;
+    document.body.appendChild(label);
 }
 
-//create a master volume slider to adjust the volume of all media tags
-function createMasterSlider() {
-	//TODO: this
-	const volumeSlider = document.createElement('input');	
-	volumeSlider.type = 'range';
-	volumeSlider.min = 0;
-	volumeSlider.max = 1;
-	volumeSlider.step = .1;
-	volumeSlider.value = 1;
-	volumeSlider.id = `slider-master`;
-	const setVolume = () => {
-		[...document.getElementsByTagName('input')].forEach(e=>{if(e.type==='range'){e.value=volumeSlider.value}});
-		runCodeOnCurrentTab(getAllValidMediaTagsCodeString+`.forEach(e=>e.volume=${volumeSlider.value})`,()=>{});
-	}
-	volumeSlider.addEventListener('change', setVolume)
-	volumeSlider.addEventListener('input', setVolume)
-	const label = document.createElement('label');
-	label.innerHTML = 'Master Volume';
-	document.body.appendChild(label);
-	document.body.appendChild(volumeSlider);
+//append a slider to the document with a label
+function createSlider(id, value, eventFunction) {
+    const volumeSlider = document.createElement('input');
+    volumeSlider.type = 'range';
+    volumeSlider.min = 0;
+    volumeSlider.max = 1;
+    volumeSlider.step = .1;
+    volumeSlider.value = value;
+    volumeSlider.id = id;
+    volumeSlider.addEventListener('change', () => eventFunction(volumeSlider.value));
+    volumeSlider.addEventListener('input', () => eventFunction(volumeSlider.value));
+    document.body.appendChild(volumeSlider);
 }
-
-//create a volume slider for each media tag and add a label with the media tag's id
-function createSliderPerMediaTag(mediaTag){
-	const volumeSlider = document.createElement('input');	
-	volumeSlider.type = 'range';
-	volumeSlider.min = 0;
-	volumeSlider.max = 1;
-	volumeSlider.step = .1;
-	volumeSlider.value = mediaTag.volume;
-	volumeSlider.id = `slider-${mediaTag.id}`;
-	const setVolume = () => runCodeOnCurrentTab(`document.getElementById('${mediaTag.id}').volume = ${volumeSlider.value}`,()=>{});
-	volumeSlider.addEventListener('change', setVolume)
-	volumeSlider.addEventListener('input', setVolume)
-	const label = document.createElement('label');
-	label.innerHTML = mediaTag.id;
-	document.body.appendChild(label);
-	document.body.appendChild(volumeSlider);
-};
 
 function runCodeOnCurrentTab(codeString, callback) {
-	chrome.tabs.query(
-		{active: true, currentWindow: true},
-		tabs=> { chrome.tabs.executeScript( tabs[0].id, {code: codeString},
-		callback)}
-	);
+    chrome.tabs.query(
+        { active: true, currentWindow: true },
+        tabs => {
+            chrome.tabs.executeScript(tabs[0].id, { code: codeString },
+                callback)
+        }
+    );
 }
+
+
